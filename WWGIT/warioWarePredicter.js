8d113aecec08057f7009ee2b22c92166e9552bdc
Ludvig Prim <ludvig@prims.se>
2024-08-04 16:57:09 +0200

    git predicter site launch

--- WWGIT/warioWarePredicter.js
+++ WWGIT/warioWarePredicter.js
@@ -14,6 +14,14 @@ const mainMenuTowersButton = document.getElementById("mainMenuTowersButton");
 const mainMenuWarioCupButton = document.getElementById("mainMenuWarioCupButton");
 
 const storyButtons = document.getElementsByClassName("storyMenuStageButton");
+const towersButtons = document.getElementsByClassName("towersMenuStageButton");
+const warioCupButtonsDiv = document.getElementById("warioCupStageButtonsDiv");
+
+const microgamesGridDocument = document.getElementById("microgamesGrid");
+const levelTextDocument = document.getElementById("levelText");
+const groupTextDocument = document.getElementById("groupText");
+const cycleTextDocument = document.getElementById("cycleText");
+const exitButton = document.getElementById("exitButton");
 
 const gameStates = 
 {
@@ -21,33 +29,97 @@ const gameStates =
     ingame: 1
 }
 
-const stageDataJson = LoadJSON();
+var stageDataJson;
 
 var gameState = gameStates.menu;
 var level = 1;
+var levelInLoop = 1;
 
 var levels = [];
 var groupDefinitions = [];
 var currentStageTitle = ''
-var repeatingGroups = false;
 var endPoint = 999;
 
 document.addEventListener("keydown", (evt) => KeyPress(evt));
-microgamesAreaDocument.addEventListener("mousedown", NextGroup);
+microgamesAreaDocument.addEventListener("mousedown", function(){
+    NextGroup(1);
+});
 
 mainMenuStoryButton.addEventListener("mousedown", (evt) => {
     mainMenuDocument.classList.add("hidden");
     storyMenuDocument.classList.remove("hidden");
 });
+mainMenuTowersButton.addEventListener("mousedown", (evt) => {
+    mainMenuDocument.classList.add("hidden");
+    towersMenuDocument.classList.remove("hidden");
+});
+mainMenuWarioCupButton.addEventListener("mousedown", (evt) => {
+    mainMenuDocument.classList.add("hidden");
+    warioCupMenuDocument.classList.remove("hidden");
+});
 
-async function LoadJSON() {
-    const response = await fetch("./stages.json");
-    const json = await response.json();
-    return json;
+function AddMouseHoverOpacity(element){
+    element.addEventListener("mouseenter", function(){
+        element.classList.add("mouseOverElement");
+    });
+    element.addEventListener("mouseleave", function(){
+        element.classList.remove("mouseOverElement");
+    });
 }
 
-for(let i = 0; i < 11; i++){
-    storyButtons[i].addEventListener("mousedown", EnterGame(stageDataJson[i]));
+setup();
+async function setup(){
+    stageDataJson = await LoadJSON();
+
+    AddMouseHoverOpacity(mainMenuStoryButton);
+    AddMouseHoverOpacity(mainMenuTowersButton);
+    AddMouseHoverOpacity(mainMenuWarioCupButton);
+
+    for(let i = 0; i < 10; i++){
+        storyButtons[i].addEventListener("mousedown", function(){
+            EnterGame(stageDataJson[i])
+        });
+        AddMouseHoverOpacity(storyButtons[i]);
+    }
+
+    for(let i = 0; i < 5; i++){
+        towersButtons[i].addEventListener("mousedown", function(){
+            EnterGame(stageDataJson[i + 10])
+        });
+        AddMouseHoverOpacity(towersButtons[i]);
+    }
+
+    for (let i = 0; i < 48; i++){
+        let container = document.createElement("div");
+        let warioCupButton = document.createElement("img");
+        let buttonTextElement = document.createElement("div");
+
+        warioCupButton.src =  (stageDataJson[i + 15].type == "Score") ? "./images/menuIcons/ScoreChallenge.png" : "./images/menuIcons/TimedChallenge.png";
+        warioCupButton.addEventListener("mousedown", function(){
+            EnterGame(stageDataJson[i + 15])
+        });
+        warioCupButton.classList.add("warioCupButton");
+
+        buttonTextElement.classList.add("warioCupButtonText");
+        buttonTextElement.innerText = stageDataJson[i + 15].title;
+        buttonTextElement.style.pointerEvents = "none";
+
+        container.appendChild(warioCupButton);
+        container.appendChild(buttonTextElement);
+        AddMouseHoverOpacity(warioCupButton);
+        warioCupButtonsDiv.appendChild(container);
+    }
+
+    for(let i = 0; i < backButtons.length; i++){
+        backButtons[i].addEventListener("mousedown", ToMainMenu);
+    }
+    exitButton.addEventListener("mousedown", ExitGame);
+}
+
+async function LoadJSON() {
+    const response = await fetch("https://ljovynn.github.io/WWGIT/stages.json");
+    const json = await response.json();
+    return json;
 }
 
 function KeyPress(evt){
@@ -69,20 +141,39 @@ function KeyPress(evt){
         case 'Escape':
             ExitGame();
     }
-    console.log(evt.key);
 }
 
 function NextGroup(skipAmount = 1){
-    console.log("next");
     if (gameState !== gameStates.ingame) return;
+
+    if (level + skipAmount > endPoint) skipAmount = endPoint - level;
+    level += skipAmount;
+    levelInLoop += skipAmount;
+    let loopsSkipped = Math.floor((levelInLoop - 1) / levels.length);
+    levelInLoop -= levels.length * loopsSkipped;
+
+    ClearLevelDisplay();
+    DisplayCurrentLevel();
 }
 
 function Back(){
     if (gameState !== gameStates.ingame) return;
+
+    if (level < 1) return;
+    level--;
+    levelInLoop--;
+    if (levelInLoop == 0) levelInLoop = levels.length;
+
+    DisplayCurrentLevel();
 }
 
 function Reset(){
     if (gameState !== gameStates.ingame) return;
+
+    level = 1;
+    levelInLoop = 1;
+
+    DisplayCurrentLevel();
 }
 
 function ExitGame(){
@@ -92,13 +183,50 @@ function ExitGame(){
     ToMainMenu();
 }
 
+function DisplayCurrentLevel(){
+    levelTextDocument.innerText = level;
+
+    if (levels[levelInLoop - 1] === ""){
+        groupTextDocument.innerText = "N/A";
+        return;
+    }
+
+    const group = groupDefinitions.find((item) => {
+        return item.id == levels[levelInLoop - 1];
+    });
+    for (let i = 0; i < group.microgames.length; i++){
+        microgamesGridDocument.children[i].src = "./images/microgames/" + group.microgames[i] + ".png";
+        microgamesGridDocument.children[i].classList.remove("hidden");
+    }
+    groupTextDocument.innerText = group.id;
+    cycleTextDocument.innerText = (group.cycle) ? group.cycle : "";
+}
+
+function ClearLevelDisplay(){
+    for (let i = 0; i < microgamesGridDocument.childElementCount; i++){
+        microgamesGridDocument.children[i].classList.add("hidden");
+    }
+}
+
 function ToMainMenu(){
     mainMenuDocument.classList.remove("hidden");
     storyMenuDocument.classList.add("hidden");
     towersMenuDocument.classList.add("hidden");
     warioCupMenuDocument.classList.add("hidden");
+    window.scrollTo(0, 0)
 }
 
-function EnterGame(stageTitle){
-    console.log(stageTitle);
+function EnterGame(stageData){
+    gameState = gameStates.ingame;
+    menuDocument.classList.add("hidden");
+    ingameDocument.classList.remove("hidden");
+
+    level = 1;
+    levelInLoop = 1;
+    levels = stageData.levels;
+    groupDefinitions = stageData.groups;
+    currentStageTitle = stageData.title;
+    endPoint = stageData.endPoint;
+
+    DisplayCurrentLevel();
 }
